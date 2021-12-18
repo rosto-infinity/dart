af424e955f864fa9802c300f667b703dbdefde87
rosto-infinity <rostoinfocus@gmail.com>
2021-12-18 04:04:29 +0100

    int double and dynamic

--- var.dart
+++ var.dart
@@ -1,5 +1,17 @@
+// void main() {
+//   String country = "canada";
+//   print("hello word! I'm from $country");
+//   print("My country has ${country.length}");
+// }
+// void main() {
+//   int age = 30;
+//   double height = 1.70;
+
+//   print("I'm $age and I'm $height m tall");
+// }
 void main() {
-  String country = "canada";
-  print("hello word! I'm from $country");
-  print("My country has ${country.length}");
+  dynamic height = 1.70;
+  height = "1.71 m";
+
+  print("I'm $height  tall");
 }
